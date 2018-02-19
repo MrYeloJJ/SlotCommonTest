6708b124d8bd4f8b98ff056e2687ab5b11e57f79
Gelomen <kai664792415@qq.com>
2018-02-19 22:41:27 +0800

    optimize RunAllTests

--- src/source/common/RunAllTests.py
+++ src/source/common/RunAllTests.py
@@ -7,21 +7,34 @@ import datetime
 from src.lib import HTMLTestReportCN
 from src.source.common.DirAndFiles import DirAndFiles
 
+
+class RunAllTests(object):
+
+    def __init__(self):
+        self.test_case_path = "../../source"
+        self.title = "SlotCommonTest Report"
+        self.description = "Slot游戏公共模块测试报告"
+        self.tester = input("请输入你的名字：")
+
+    def run(self):
+        test_suite = unittest.TestLoader().discover(self.test_case_path)
+
+        # 启动测试时创建文件夹并获取最新文件夹的名字
+        daf = DirAndFiles()
+        daf.create_dir()
+        new_dir = daf.get_new_dir()
+
+        # 在最新文件夹下新建测试报告
+        now = str(datetime.datetime.now().strftime("%Y-%m-%d(%H-%M-%S)"))
+        file_path = new_dir + "/SlotCommonTest_report_" + now + ".html"
+
+        fp = open(file_path, "wb")
+        runner = HTMLTestReportCN.HTMLTestRunner(stream=fp, title=self.title, description=self.description, tester=self.tester)
+        runner.run(test_suite)
+        fp.close()
+
+        print("\033[36;0m--------------------- 测试结束 ---------------------\033[0m")
+
+
 if __name__ == "__main__":
-    test_suite = unittest.TestLoader().discover("../../source")
-
-    # 启动测试时创建文件夹并获取最新文件夹的名字
-    daf = DirAndFiles()
-    daf.create_dir()
-    new_dir = daf.get_new_dir()
-
-    # 在最新文件夹下新建测试报告
-    now = str(datetime.datetime.now().strftime("%Y-%m-%d(%H-%M-%S)"))
-    file_path = new_dir + "/SlotCommonTest_report_" + now + ".html"
-
-    fp = open(file_path, "wb")
-    runner = HTMLTestReportCN.HTMLTestRunner(stream=fp, title="SlotCommonTest Report", description="Slot游戏公共模块测试报告", tester="Gelomen")
-    runner.run(test_suite)
-    fp.close()
-
-    print("\033[36;0m--------------------- 测试结束 ---------------------\033[0m")
+    RunAllTests().run()
