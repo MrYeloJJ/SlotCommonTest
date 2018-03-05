71eb7841c1e623935f6278eb7c2fd8f4ec0d67e8
Gelomen <kai664792415@qq.com>
2018-03-05 17:01:53 +0800

    update

--- src/source/common/RunAllTests.py
+++ src/source/common/RunAllTests.py
@@ -5,13 +5,18 @@
 import unittest
 import datetime
 from src.lib import HTMLTestReportCN
+from src.source.common.Data import Data
 
 
 class RunAllTests(object):
 
     def __init__(self):
         self.test_case_path = "../../source"
-        self.title = "SlotCommonTest Report"
+
+        # 将游戏名字放入报告里
+        self.game = Data().get_message()["game"]
+        self.title = "【" + self.game + "】公共模块测试报告"
+
         self.description = "Slot游戏公共模块测试报告"
 
     def run(self):
@@ -23,8 +28,8 @@ class RunAllTests(object):
         new_dir = daf.get_new_dir()
 
         # 在最新文件夹下新建测试报告
-        now = str(datetime.datetime.now().strftime("%Y-%m-%d(%H-%M-%S)"))
-        file_path = new_dir + "/SlotCommonTest_report_" + now + ".html"
+        now = str(datetime.datetime.now().strftime("%Y{y}%m{m}%d{d}(%H{H}%M{M}%S{S})").format(y="年", m="月", d="日", H="时", M="分", S="秒"))
+        file_path = new_dir + "/【" + self.game + "】公共模块测试报告_" + now + ".html"
 
         fp = open(file_path, "wb")
         runner = HTMLTestReportCN.HTMLTestRunner(stream=fp, need_screenshot=1, title=self.title, description=self.description, tester=input("请输入你的名字："))
