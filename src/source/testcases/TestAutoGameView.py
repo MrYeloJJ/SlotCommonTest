14c8408d1507060f9103738250c3d1ac70795a2d
Gelomen <kai664792415@qq.com>
2018-03-31 10:09:24 +0800

    update

--- src/source/testcases/TestAutoGameView.py
+++ src/source/testcases/TestAutoGameView.py
@@ -309,7 +309,7 @@ class TestAutoGameView(unittest.TestCase):
             self.assertEqual(view_dispear, None, "横屏启动自动游戏，自动游戏设置面板不会消失！")
             self.assertEqual(mask_dispear, False, "横屏启动自动游戏，灰色蒙板不会消失！")
             self.assertEqual(slot_rolling, True, "横屏启动自动游戏，滚轴不会滚动！")
-            self.assertEqual(start_btn_status, "playing", "横屏启动自动游戏，旋转按钮不会变成停止状态！")
+            self.assertEqual(start_btn_status, "playing", "横屏启动自动游戏，旋转按钮不会变成停止按钮状态！")
             self.assertEqual(setting_btn, False, "横屏启动自动游戏，线数线注设置按钮不会消失！")
             self.assertEqual(auto_game_btn, False, "横屏启动自动游戏，自动游戏按钮不会消失！")
             self.assertEqual(main_menu_expand, "retractL", "横屏启动自动游戏，左侧选项菜单不会折叠！")
@@ -345,7 +345,7 @@ class TestAutoGameView(unittest.TestCase):
                     if slot_status:
                         break
 
-                # 获取停止旋转按钮上的剩余次数
+                # 获取停止旋转按钮和下导航栏上的剩余次数
                 current_spin_time = self.common.in_auto_spin_btn_text()
                 current_info_bar_spin_time = self.common.info_bar_view_banner_tips_label()
 
@@ -371,6 +371,77 @@ class TestAutoGameView(unittest.TestCase):
 
             self.common.start_btn_click()
             sleep(1)
+
+    # 验证横屏 自动次数为0时停止
+    def test_auto_spin_time_is_zero(self):
+        self.common.loading_bar()
+        sleep(1)
+        self.common.sound_view_yes_btn_click()
+        sleep(1)
+
+        self.common.auto_game_btn_click()
+        sleep(1)
+        self.common.auto_game_view_change_auto_time(0)
+        sleep(1)
+
+        target_spin_btn_time = self.common.auto_game_times[0]
+
+        self.common.auto_game_view_start_btn_click()
+        sleep(1)
+
+        while True:
+
+            while True:
+                slot_status = self.common.slot_machine_rolling()
+                if slot_status:
+                    break
+
+            # 获取停止旋转按钮和下导航栏上的剩余次数
+            current_spin_time = self.common.in_auto_spin_btn_text()
+            current_info_bar_spin_time = self.common.info_bar_view_banner_tips_label()
+
+            if target_spin_btn_time is -1 or target_spin_btn_time is "直到":
+                target_spin_btn_time = "直到"
+                # 获取下导航栏提示文字
+                target_info_bar_spin_time = "直到环节自动旋转"
+            else:
+                target_spin_btn_time = target_spin_btn_time - 1
+                target_info_bar_spin_time = "剩余" + str(target_spin_btn_time) + "次自动旋转"
+
+            try:
+                self.assertEqual(current_spin_time, str(target_spin_btn_time), "横屏启动自动游戏，停止按钮上的剩余次数错误！")
+                self.assertEqual(current_info_bar_spin_time, target_info_bar_spin_time, "横屏启动自动游戏，下导航栏上的剩余次数错误！")
+            except AssertionError:
+                self.daf.get_screenshot(self.browser)
+                raise
+
+            if current_spin_time is "0":
+                while True:
+                    slot_status = self.common.slot_machine_rolling()
+                    if slot_status is False:
+                        for i in range(20):
+                            # 获取滚轴滚动状态
+                            slot_rolling = self.common.slot_machine_rolling()
+                            # 获取旋转按钮状态
+                            start_btn_status = self.common.start_btn_status()
+                            # 获取线数线注按钮、自动游戏按钮、选项菜单状态
+                            setting_btn = self.common.setting_btn_visible()
+                            auto_game_btn = self.common.auto_game_btn_visible()
+                            main_menu_expand = self.common.main_menu_expand()
+                            main_menu = self.common.main_menu_touchable()
+
+                            try:
+                                self.assertEqual(slot_rolling, False, "横屏自动游戏次数为0后，滚轴不会停止滚动！")
+                                self.assertEqual(start_btn_status, "stopped", "横屏自动游戏次数为0后，旋转按钮不会变成旋转按钮状态！")
+                                self.assertEqual(setting_btn, True, "横屏自动游戏次数为0后，线数线注设置按钮不会重新显示！")
+                                self.assertEqual(auto_game_btn, True, "横屏自动游戏次数为0后，自动游戏按钮不会重新显示！")
+                                self.assertEqual(main_menu_expand, "retractL", "横屏自动游戏次数为0后，左侧选项菜单不会依然折叠！")
+                                self.assertEqual(main_menu, True, "横屏自动游戏次数为0后，左侧选项菜单不可以点击！")
+                            except AssertionError:
+                                self.daf.get_screenshot(self.browser)
+                                raise
+                        break
+
 
 
 
