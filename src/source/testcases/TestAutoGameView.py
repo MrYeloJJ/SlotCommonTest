fa7aaa44179051c1d029b94998fd1614418078c4
Gelomen <kai664792415@qq.com>
2018-03-31 11:30:28 +0800

    update

--- src/source/testcases/TestAutoGameView.py
+++ src/source/testcases/TestAutoGameView.py
@@ -384,8 +384,6 @@ class TestAutoGameView(unittest.TestCase):
         self.common.auto_game_view_change_auto_time(0)
         sleep(1)
 
-        target_spin_btn_time = self.common.auto_game_times[0]
-
         self.common.auto_game_view_start_btn_click()
         sleep(1)
 
@@ -396,24 +394,8 @@ class TestAutoGameView(unittest.TestCase):
                 if slot_status:
                     break
 
-            # 获取停止旋转按钮和下导航栏上的剩余次数
+            # 获取停止旋转按钮上的剩余次数
             current_spin_time = self.common.in_auto_spin_btn_text()
-            current_info_bar_spin_time = self.common.info_bar_view_banner_tips_label()
-
-            if target_spin_btn_time is -1 or target_spin_btn_time is "直到":
-                target_spin_btn_time = "直到"
-                # 获取下导航栏提示文字
-                target_info_bar_spin_time = "直到环节自动旋转"
-            else:
-                target_spin_btn_time = target_spin_btn_time - 1
-                target_info_bar_spin_time = "剩余" + str(target_spin_btn_time) + "次自动旋转"
-
-            try:
-                self.assertEqual(current_spin_time, str(target_spin_btn_time), "横屏启动自动游戏，停止按钮上的剩余次数错误！")
-                self.assertEqual(current_info_bar_spin_time, target_info_bar_spin_time, "横屏启动自动游戏，下导航栏上的剩余次数错误！")
-            except AssertionError:
-                self.daf.get_screenshot(self.browser)
-                raise
 
             if current_spin_time is "0":
                 while True:
@@ -440,6 +422,27 @@ class TestAutoGameView(unittest.TestCase):
                             except AssertionError:
                                 self.daf.get_screenshot(self.browser)
                                 raise
+                        break
+                break
+            else:
+                while True:
+                    slot_status = self.common.slot_machine_rolling()
+                    if slot_status is False:
+                        game_status = self.common.get_game_current_status()
+                        if game_status is not None:
+                            self.browser.refresh()
+                            self.common.loading_bar()
+                            sleep(1)
+                            self.common.sound_view_yes_btn_click()
+                            sleep(1)
+
+                            self.common.auto_game_btn_click()
+                            sleep(1)
+                            self.common.auto_game_view_change_auto_time(0)
+                            sleep(1)
+
+                            self.common.auto_game_view_start_btn_click()
+                            sleep(1)
                         break
 
 
