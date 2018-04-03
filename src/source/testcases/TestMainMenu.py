d6538e6573cbce03c6c2e92f764f6a24335664c0
Gelomen <kai664792415@qq.com>
2018-04-03 13:57:48 +0800

    update

--- src/source/testcases/TestMainMenu.py
+++ src/source/testcases/TestMainMenu.py
@@ -26,25 +26,14 @@ class TestMainMenu(unittest.TestCase):
     #
     #
 
-    # 验证横屏显示左侧主菜单按钮
-    def test_main_menu_btn_visible(self):
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        visible = self.common.main_menu_btn_visible()
-        try:
-            self.assertEqual(visible, True, "横屏不会显示主菜单按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
     # 验证横屏左侧主菜单默认为展开状态
     def test_main_menu_default_expand(self):
         self.common.loading_bar()
         sleep(1)
         self.common.sound_view_yes_btn_click()
         sleep(1)
+        # 显示
+        main_menu = self.common.main_menu_btn_visible()
         expand = self.common.main_menu_expand()
         info_btn = self.common.info_btn_visible()
         help_btn = self.common.help_btn_visible()
@@ -52,7 +41,14 @@ class TestMainMenu(unittest.TestCase):
         turbo_btn = self.common.turbo_btn_visible()
         home_btn = self.common.home_btn_visible()
         game_record_btn = self.common.game_record_btn_enable()
+        # 可点击否
+        main_menu_touchable = self.common.main_menu_touchable()
+        info_btn_touchable = self.common.info_btn_touchable()
+        help_btn_touchable = self.common.help_btn_touchable()
+        voice_btn_touchable = self.common.voice_btn_touchable()
+        turbo_btn_touchable = self.common.turbo_btn_touchable()
         try:
+            self.assertEqual(main_menu, True, "横屏不会显示主菜单按钮！")
             self.assertEqual(expand, "expandLPC", "横屏左侧主菜单默认不是展开状态！")
             self.assertEqual(info_btn, True, "横屏左侧主菜单展开时不会显示奖金表按钮！")
             self.assertEqual(help_btn, True, "横屏左侧主菜单展开时不会显示帮助按钮！")
@@ -60,19 +56,11 @@ class TestMainMenu(unittest.TestCase):
             self.assertEqual(turbo_btn, True, "横屏左侧主菜单展开时不会显示快速按钮！")
             self.assertEqual(home_btn, False, "横屏PC端左侧主菜单展开时，会显示返回大厅按钮！")
             self.assertEqual(game_record_btn, False, "横屏试玩左侧主菜单展开时，会显示游戏记录按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
-    # 验证横屏整个左侧主菜单可点击否
-    def test_main_menu_touchable(self):
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        touchable = self.common.main_menu_touchable()
-        try:
-            self.assertEqual(touchable, True, "横屏整个左侧主菜单不能点击！")
+            self.assertEqual(main_menu_touchable, True, "横屏整个左侧主菜单不能点击！")
+            self.assertEqual(info_btn_touchable, True, "横屏奖金表按钮不能点击！")
+            self.assertEqual(help_btn_touchable, True, "横屏帮助按钮不可点击！")
+            self.assertEqual(voice_btn_touchable, True, "横屏声音开关按钮不可点击！")
+            self.assertEqual(turbo_btn_touchable, True, "横屏快速模式按钮不可点击！")
         except AssertionError:
             self.daf.get_screenshot(self.browser)
             raise
@@ -129,32 +117,6 @@ class TestMainMenu(unittest.TestCase):
             self.assertEqual(turbo_btn, True, "横屏点击左侧主菜单按钮两次后不会显示快速按钮！")
             self.assertEqual(home_btn, False, "横屏PC端点击左侧主菜单按钮两次后，会显示返回大厅按钮！")
             self.assertEqual(game_record_btn, False, "横屏试玩点击左侧主菜单按钮两次后，会显示游戏记录按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
-    # 验证横屏显示奖金表按钮
-    def test_info_btn_visible(self):
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        visible = self.common.info_btn_visible()
-        try:
-            self.assertEqual(visible, True, "横屏奖金表按钮默认不会显示！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
-    # 验证横屏奖金表按钮可点击否
-    def test_info_btn_touchable(self):
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        touchable = self.common.info_btn_touchable()
-        try:
-            self.assertEqual(touchable, True, "横屏奖金表按钮不能点击！")
         except AssertionError:
             self.daf.get_screenshot(self.browser)
             raise
@@ -221,32 +183,6 @@ class TestMainMenu(unittest.TestCase):
             self.daf.get_screenshot(self.browser)
             raise
 
-    # 验证横屏显示帮助按钮
-    def test_help_btn_visible(self):
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        visible = self.common.help_btn_visible()
-        try:
-            self.assertEqual(visible, True, "横屏不会显示帮助按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
-    # 验证横屏帮助按钮可点击否
-    def test_help_btn_touchable(self):
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        touchable = self.common.help_btn_touchable()
-        try:
-            self.assertEqual(touchable, True, "横屏帮助按钮不可点击！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
     # 验证横屏点击帮助按钮后，显示帮助场景
     def test_help_btn_click_help_view_showing(self):
         self.common.loading_bar()
@@ -309,32 +245,6 @@ class TestMainMenu(unittest.TestCase):
             self.daf.get_screenshot(self.browser)
             raise
 
-    # 验证横屏显示声音开关按钮
-    def test_voice_btn_visible(self):
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        visible = self.common.voice_btn_visible()
-        try:
-            self.assertEqual(visible, True, "横屏不会显示声音开关按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
-    # 验证横屏声音开关按钮可点击否
-    def test_voice_btn_touchable(self):
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        touchable = self.common.voice_btn_touchable()
-        try:
-            self.assertEqual(touchable, True, "横屏声音开关按钮不可点击！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
     # 验证横屏声音开关按钮点击后，按钮状态改变，声音关闭
     def test_voice_btn_click_status(self):
         self.common.loading_bar()
@@ -371,19 +281,6 @@ class TestMainMenu(unittest.TestCase):
             self.daf.get_screenshot(self.browser)
             raise
 
-    # 验证横屏显示快速模式按钮
-    def test_turbo_btn_visible(self):
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        visible = self.common.turbo_btn_visible()
-        try:
-            self.assertEqual(visible, True, "横屏不会显示快速模式按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
     # 验证横屏快速模式按钮默认状态为1x，滚轴滚动方式是普通速度
     def test_turbo_btn_default_status(self):
         self.common.loading_bar()
@@ -395,19 +292,6 @@ class TestMainMenu(unittest.TestCase):
         try:
             self.assertEqual(turbo_btn_status, "1x", "横屏快速模式按钮默认状态不是1x")
             self.assertEqual(spin_status, False, "横屏滚轴默认滚动方式不是普通速度！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
-    # 验证横屏快速模式按钮可点击否
-    def test_turbo_btn_touchable(self):
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        touchable = self.common.turbo_btn_touchable()
-        try:
-            self.assertEqual(touchable, True, "横屏快速模式按钮不可点击！")
         except AssertionError:
             self.daf.get_screenshot(self.browser)
             raise
@@ -429,51 +313,11 @@ class TestMainMenu(unittest.TestCase):
             self.daf.get_screenshot(self.browser)
             raise
 
-    # 验证横屏PC端显示返回大厅按钮
-    def test_home_btn_visible(self):
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        visible = self.common.home_btn_visible()
-        try:
-            self.assertEqual(visible, False, "横屏PC端会显示返回大厅按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
-    # 验证横屏试玩显示游戏记录按钮
-    def test_game_record_btn_enable(self):
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        enable = self.common.game_record_btn_enable()
-        try:
-            self.assertEqual(enable, False, "横屏试玩会显示游戏记录按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise 
-
     #
     #
     # ------------------------------------------------------------------------ 竖屏模式 ------------------------------------------------------------------------
     #
     #
-
-    # 验证竖屏显示左侧主菜单按钮
-    def test_main_menu_btn_visible_portrait(self):
-        self.common.portrait()
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        visible = self.common.main_menu_btn_visible()
-        try:
-            self.assertEqual(visible, True, "竖屏不会显示主菜单按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
 
     # 验证竖屏左侧主菜单默认为展开状态
     def test_main_menu_default_expand_portrait(self):
@@ -482,6 +326,8 @@ class TestMainMenu(unittest.TestCase):
         sleep(1)
         self.common.sound_view_yes_btn_click()
         sleep(1)
+        # 显示
+        main_menu = self.common.main_menu_btn_visible()
         expand = self.common.main_menu_expand()
         info_btn = self.common.info_btn_visible()
         help_btn = self.common.help_btn_visible()
@@ -489,7 +335,14 @@ class TestMainMenu(unittest.TestCase):
         turbo_btn = self.common.turbo_btn_visible()
         home_btn = self.common.home_btn_visible()
         game_record_btn = self.common.game_record_btn_enable()
+        # 可点击否
+        main_menu_touchable = self.common.main_menu_touchable()
+        info_btn_touchable = self.common.info_btn_touchable()
+        help_btn_touchable = self.common.help_btn_touchable()
+        voice_btn_touchable = self.common.voice_btn_touchable()
+        turbo_btn_touchable = self.common.turbo_btn_touchable()
         try:
+            self.assertEqual(main_menu, True, "竖屏不会显示主菜单按钮！")
             self.assertEqual(expand, "expandPPC", "竖屏左侧主菜单默认不是展开状态！")
             self.assertEqual(info_btn, True, "竖屏左侧主菜单展开时不会显示奖金表按钮！")
             self.assertEqual(help_btn, True, "竖屏左侧主菜单展开时不会显示帮助按钮！")
@@ -497,20 +350,11 @@ class TestMainMenu(unittest.TestCase):
             self.assertEqual(turbo_btn, True, "竖屏左侧主菜单展开时不会显示快速按钮！")
             self.assertEqual(home_btn, False, "竖屏PC端上方主菜单展开时，会显示返回大厅按钮！")
             self.assertEqual(game_record_btn, False, "竖屏试玩上方主菜单展开时，会显示游戏记录按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
-    # 验证竖屏整个左侧主菜单可点击否
-    def test_main_menu_touchable_portrait(self):
-        self.common.portrait()
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        touchable = self.common.main_menu_touchable()
-        try:
-            self.assertEqual(touchable, True, "竖屏整个左侧主菜单不能点击！")
+            self.assertEqual(main_menu_touchable, True, "横屏整个左侧主菜单不能点击！")
+            self.assertEqual(info_btn_touchable, True, "横屏奖金表按钮不能点击！")
+            self.assertEqual(help_btn_touchable, True, "横屏帮助按钮不可点击！")
+            self.assertEqual(voice_btn_touchable, True, "横屏声音开关按钮不可点击！")
+            self.assertEqual(turbo_btn_touchable, True, "横屏快速模式按钮不可点击！")
         except AssertionError:
             self.daf.get_screenshot(self.browser)
             raise
@@ -569,34 +413,6 @@ class TestMainMenu(unittest.TestCase):
             self.assertEqual(turbo_btn, True, "竖屏点击左侧主菜单按钮两次后不会显示快速按钮！")
             self.assertEqual(home_btn, False, "竖屏PC端点击左侧主菜单按钮两次后，会显示返回大厅按钮！")
             self.assertEqual(game_record_btn, False, "竖屏试玩点击左侧主菜单按钮两次后，会显示游戏记录按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
-    # 验证竖屏显示奖金表按钮
-    def test_info_btn_visible_portrait(self):
-        self.common.portrait()
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        visible = self.common.info_btn_visible()
-        try:
-            self.assertEqual(visible, True, "竖屏奖金表按钮默认不会显示！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
-    # 验证竖屏奖金表按钮可点击否
-    def test_info_btn_touchable_portrait(self):
-        self.common.portrait()
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        touchable = self.common.info_btn_touchable()
-        try:
-            self.assertEqual(touchable, True, "竖屏奖金表按钮不能点击！")
         except AssertionError:
             self.daf.get_screenshot(self.browser)
             raise
@@ -667,34 +483,6 @@ class TestMainMenu(unittest.TestCase):
             self.daf.get_screenshot(self.browser)
             raise
 
-    # 验证竖屏显示帮助按钮
-    def test_help_btn_visible_portrait(self):
-        self.common.portrait()
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        visible = self.common.help_btn_visible()
-        try:
-            self.assertEqual(visible, True, "竖屏不会显示帮助按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
-    # 验证竖屏帮助按钮可点击否
-    def test_help_btn_touchable_portrait(self):
-        self.common.portrait()
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        touchable = self.common.help_btn_touchable()
-        try:
-            self.assertEqual(touchable, True, "竖屏帮助按钮不可点击！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
     # 验证竖屏点击帮助按钮后，显示帮助场景
     def test_help_btn_click_help_view_showing_portrait(self):
         self.common.portrait()
@@ -761,34 +549,6 @@ class TestMainMenu(unittest.TestCase):
             self.daf.get_screenshot(self.browser)
             raise
 
-    # 验证竖屏显示声音开关按钮
-    def test_voice_btn_visible_portrait(self):
-        self.common.portrait()
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        visible = self.common.voice_btn_visible()
-        try:
-            self.assertEqual(visible, True, "竖屏不会显示声音开关按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
-    # 验证竖屏声音开关按钮可点击否
-    def test_voice_btn_touchable_portrait(self):
-        self.common.portrait()
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        touchable = self.common.voice_btn_touchable()
-        try:
-            self.assertEqual(touchable, True, "竖屏声音开关按钮不可点击！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
     # 验证竖屏声音开关按钮点击后，按钮状态改变，声音关闭
     def test_voice_btn_click_status_portrait(self):
         self.common.portrait()
@@ -827,20 +587,6 @@ class TestMainMenu(unittest.TestCase):
             self.daf.get_screenshot(self.browser)
             raise
 
-    # 验证竖屏显示快速模式按钮
-    def test_turbo_btn_visible_portrait(self):
-        self.common.portrait()
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        visible = self.common.turbo_btn_visible()
-        try:
-            self.assertEqual(visible, True, "竖屏不会显示快速模式按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
     # 验证竖屏快速模式按钮默认状态为1x，滚轴滚动方式是普通速度
     def test_turbo_btn_default_status_portrait(self):
         self.common.portrait()
@@ -853,20 +599,6 @@ class TestMainMenu(unittest.TestCase):
         try:
             self.assertEqual(turbo_btn_status, "1x", "竖屏快速模式按钮默认状态不是1x")
             self.assertEqual(spin_status, False, "竖屏滚轴默认滚动方式不是普通速度！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
-    # 验证竖屏快速模式按钮可点击否
-    def test_turbo_btn_touchable_portrait(self):
-        self.common.portrait()
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        touchable = self.common.turbo_btn_touchable()
-        try:
-            self.assertEqual(touchable, True, "竖屏快速模式按钮不可点击！")
         except AssertionError:
             self.daf.get_screenshot(self.browser)
             raise
@@ -889,58 +621,11 @@ class TestMainMenu(unittest.TestCase):
             self.daf.get_screenshot(self.browser)
             raise
 
-    # 验证竖屏PC端显示返回大厅按钮
-    def test_home_btn_visible_portrait(self):
-        self.common.portrait()
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        visible = self.common.home_btn_visible()
-        try:
-            self.assertEqual(visible, False, "竖屏PC端会显示返回大厅按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
-    # 验证竖屏试玩显示游戏记录按钮
-    def test_game_record_btn_enable_portrait(self):
-        self.common.portrait()
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        enable = self.common.game_record_btn_enable()
-        try:
-            self.assertEqual(enable, False, "竖屏试玩会显示游戏记录按钮！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
-
     #
     #
     # ------------------------------------------------------------------------ 横竖屏模式 ------------------------------------------------------------------------
     #
     #
-
-    # 验证横竖屏切换 主菜单按钮显示
-    def test_main_menu_btn_visible_switch_screen(self):
-        self.common.portrait()
-        self.common.loading_bar()
-        sleep(1)
-        self.common.sound_view_yes_btn_click()
-        sleep(1)
-        self.common.landscape()
-        visible1 = self.common.main_menu_btn_visible()
-        sleep(1)
-        self.common.portrait()
-        sleep(1)
-        visible2 = self.common.main_menu_btn_visible()
-        try:
-            self.assertEqual(visible1, visible2, "横竖屏切换，主菜单按钮显示状态不一致！")
-        except AssertionError:
-            self.daf.get_screenshot(self.browser)
-            raise
 
     # 验证横竖屏切换 左侧主菜单展开状态
     def test_main_menu_default_expand_switch_screen(self):
