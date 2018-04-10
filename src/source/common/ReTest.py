a1d91400076f0a343fc086d2c15d45f4e21562cd
Gelomen <kai664792415@qq.com>
2018-04-10 17:50:33 +0800

    update

--- src/source/common/ReTest.py
+++ src/source/common/ReTest.py
@@ -3,7 +3,6 @@
 """"" 重新验证失败和错误的用例，引入不通过的测试类，修改对应的用例名字 """""
 
 import unittest
-from datetime import datetime
 import sys
 from src.source.testcases.TestLoadingView import TestLoadingView
 from src.lib.HTMLTestReportCN import DirAndFiles
@@ -12,8 +11,6 @@ from src.lib.HTMLTestReportCN import DirAndFiles
 if __name__ == "__main__":
     # 启动测试时创建文件夹
     DirAndFiles().create_dir()
-
-    startTime = datetime.now()
 
     # 重复测试n遍
     n = 1
@@ -29,6 +26,4 @@ if __name__ == "__main__":
 
         print("\n")
 
-    stopTime = datetime.now()
-    print("\n\033[36;0m--------------------- 测试结束 ---------------------\n"
-          "------------- 合计耗时: %s -------------\033[0m" % (stopTime - startTime), file=sys.stderr)
+    print("\n\033[36;0m--------------------- 测试结束 ---------------------\n", file=sys.stderr)
