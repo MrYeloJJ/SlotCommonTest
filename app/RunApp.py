4b2e516e174715183555e3d3976d50e682336932
gelomen <kai664792415@qq.com>
2018-06-04 11:10:55 +0800

    update

--- app/RunApp.py
+++ app/RunApp.py
@@ -3,7 +3,7 @@
 from multiprocessing import Process
 import os
 from flask import Flask, request, jsonify, redirect
-from app.main.AllReportsName import AllReportsName
+from app.main.SlotReport import SlotReport
 from app.main.GameAttr import GameAttr
 from app.main.TestCaseDoc import TestCaseDoc
 from app.automaticTest.slot.source.common.RunAllTests import RunAllTests
@@ -24,7 +24,7 @@ def run_all_slot_tests():
     p = Process(target=run_slot_all, args=(data_json,))
     p.start()
     p.join()
-    return jsonify({"code": 200, "tip": "测试结束"}), 200
+    return jsonify({"code": 200, "msg": "测试结束"}), 200
 
 
 def run_slot_all(data_json):
@@ -39,7 +39,7 @@ def stop_all_tests(pid):
     print("目标进程：" + str(pid))
     # os.kill(int(pid), 9)
     os.system("taskkill /pid " + str(pid) + " -t -f")
-    return jsonify({"code": 200, "tip": "测试停止"}), 200
+    return jsonify({"code": 200, "msg": "测试停止"}), 200
 
 
 # 获取slot所有用例名字和描述
@@ -51,7 +51,7 @@ def all_slot_test_doc():
 # 获取slot所有测试报告
 @app.route("/slot/allReports", methods=["GET"])
 def all_slot_reports():
-    report_name = AllReportsName().get_slot_report_url()
+    report_name = SlotReport().get_slot_report_url()
     return jsonify(report_name)
 
 
@@ -59,6 +59,13 @@ def all_slot_reports():
 @app.route("/slot/report/<report_name>", methods=["GET"])
 def slot_report(report_name):
     return redirect("./static/slot/" + str(report_name) + "/" + str(report_name) + ".html")
+
+
+# 删除slot报告
+@app.route("/slot/delete_report/<report_name>", methods=["GET"])
+def delete_slot_report(report_name):
+    SlotReport().delete_report(report_name)
+    return jsonify({"code": 200, "msg": "删除成功"}), 200
 
 
 if __name__ == "__main__":
--- app/main/SlotReport.py
+++ app/main/SlotReport.py
@@ -1,9 +1,10 @@
 # encoding=utf-8
 
 import os
+import shutil
 
 
-class AllReportsName(object):
+class SlotReport(object):
     def __init__(self):
         self.root_url = "http://127.0.0.1:5000/"
         self.slot_report_path = "./static/slot/"
@@ -14,8 +15,18 @@ class AllReportsName(object):
             report_dict = {}
             if i.startswith("【"):
                 url = self.root_url + "slot/report/" + i
+                del_url = self.root_url + "slot/delete_report/" + i
                 report_dict["report_name"] = i
                 report_dict["url"] = url
+                report_dict["del_url"] = del_url
                 self.report_list.append(report_dict)
 
         return self.report_list
+
+    def delete_report(self, report_name):
+        for i in os.listdir(self.slot_report_path):
+            if i == report_name:
+                try:
+                    shutil.rmtree(self.slot_report_path + report_name)
+                except Exception:
+                    raise
